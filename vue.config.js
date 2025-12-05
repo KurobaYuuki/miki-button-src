e34868b7a4fee8fc5e29f242b27086019ffe72b2
KurobaYuuki <78408086+KurobaYuuki@users.noreply.github.com>
2025-12-05 23:43:53 +0800

    Compatible up to Node.js v22

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,18 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
       })
     ]
+  },
+  chainWebpack: config => {
+    // See: https://github.com/vuejs/vue-cli/issues/5753
+    // Prevents "Multiple assets emit different content to the same filename index.html"
+    config.plugin('copy').tap(args => {
+      args[0].patterns[0].globOptions.ignore.push('**/index.html');
+      return args;
+    });
   }
 }
